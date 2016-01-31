7a638b31498fd049efee6a1960a66b0f35aed238
Ethan Seither <ethan.seither@gmail.com>
2016-01-30 20:22:11 -0800

    mebbe

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@ from app_start import *
 import os
 
 if __name__ == "__main__":
-    debug = False
+    debug = True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=debug)
